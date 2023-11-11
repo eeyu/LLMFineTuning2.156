a4f420be4f6855d93a21b8a949c0cb8772bbba08
Nomi <yunomi@mit.edu>
2023-11-11 15:20:01 -0500

    wikipedia extractor explores multiple depths

--- wikipedia/main.py
+++ wikipedia/main.py
@@ -42,7 +42,9 @@ def get_n_categories_of_page(page, n):
 
 # Returns False for any generic category ex. "articles containing ___"
 def normal_category_filter(c_title):
-    bad_words = ["article", "Article", "Wikipedia", "dates", "Pages", "Wikidata", "reference errors", "Webarchive", "errors:"]
+    bad_words = ["article", "Article", "Wikipedia", "dates", "Pages", "Wikidata",
+                 "reference errors", "Webarchive", "errors:", "Source attribution",
+                 "maint:", "pages", "CS1"]
     condition = True
     for word in bad_words:
         condition = condition and (word not in c_title)
@@ -66,11 +68,10 @@ def get_n_pages_from_category(category, n):
 
 
 # Returns n pages that share categories with the given page
-def get_n_links_from_page_categories(page, n):
-    num_cat = 5
-    num_extract_per_cat = int(n / num_cat)
+def get_n_links_from_page_categories(page, n, num_categories=5):
+    num_extract_per_cat = int(n / num_categories)
 
-    categories, c_titles = get_n_categories_of_page(page, num_cat)
+    categories, c_titles = get_n_categories_of_page(page, num_categories)
 
     links = []
     link_titles = []
@@ -83,15 +84,21 @@ def get_n_links_from_page_categories(page, n):
     return links, link_titles
 
 def save_page(folder, page, index="0"):
+    # Subfolder for this save
     subpath = paths.WIKIPEDIA_DATA_PATH + str(index) + "/"
     if not os.path.isdir(subpath):
         os.mkdir(subpath)
+    # holds extractions
     path = subpath + clean_name(folder) + "/"
     if not os.path.isdir(path):
         os.mkdir(path)
-    name = page.title
+    # Holds uneditted extractions
+    path_uneditted = path + "u/"
+    if not os.path.isdir(path_uneditted):
+        os.mkdir(path_uneditted)
 
-    with open(path + clean_name(name) + "_u.txt", 'w') as f:
+    name = page.title
+    with open(path_uneditted + clean_name(name) + "_u.txt", 'w') as f:
         f.write(page.text)
     with open(path + clean_name(name) + ".txt", 'w') as f:
         text = tp.remove_text_after_sections(["References", "See also"], page.text)
@@ -102,29 +109,40 @@ def save_page(folder, page, index="0"):
 def clean_name(name):
     return name.replace("/", "", 10)
 
+# returns n^depth pages
+# Algorithm:
+# At each depth, sample n linked pages. linked pages will be from related categories and direct page links
+# For each linked page, save and sample n more pages
+def get_n_links_from_page_with_depth(page, n, exploit_explore_ratio=0.5, max_depth=1, cur_depth = 0, save_index="0"):
+    n_direct_links = int(exploit_explore_ratio * n)
+    n_category_links = n - n_direct_links
+
+    links_c, _ = get_n_links_from_page_categories(page, n_category_links)
+    links_d, _ = get_n_links_from_page(page, n_direct_links)
+    links = links_d + links_c
+
+    for l in links:
+        save_page("", l, index=save_index)
+    if cur_depth + 1 == max_depth:
+        return
+    else:
+        for l in links:
+            get_n_links_from_page_with_depth(l, n, exploit_explore_ratio, max_depth, cur_depth + 1, save_index)
+
+
 if __name__ == "__main__":
     wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)',
                                        'en',
                                        extract_format=wikipediaapi.ExtractFormat.WIKI)
 
-    p_wiki = wiki_wiki.page('gravity')
-
-    print("Origin Page - Summary: %s" % p_wiki.summary[0:60])
-
-    links_c, l_c_titles = get_n_links_from_page_categories(p_wiki, 10)
-    links, l_titles = get_n_links_from_page(p_wiki, 10)
+    origin_page_titles = ['Technical standard', 'IEEE Standards Association', 'List of technical standard organizations']
 
     now = datetime.datetime.now()
     save_index = now.strftime('%h_%m_%s')
 
-    # random_save = random.randint(0, 100000)
+    for title in origin_page_titles:
+        p_wiki = wiki_wiki.page(title)
+        print("Origin Page - Summary: %s" % p_wiki.summary[0:60])
 
-    for l in links_c:
-        print("link_c: " + l.title)
-        save_page("", l, index=save_index)
-
-    for l in links:
-        print("link: " + l.title)
-        save_page("", l, index=save_index)
-
+        get_n_links_from_page_with_depth(page=p_wiki, n=7, exploit_explore_ratio=0.5, max_depth=2, cur_depth=0, save_index=save_index)
 
